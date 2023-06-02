ff738c95ffb1f8358df09ce1ca26249b39b6c571
Siu Wun Cheung <cheung26@llnl.gov>
2023-06-02 07:11:22 -0700

    Add scripts

---
+++ run_main_AE_GFINNs_simul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -nnodes 1
+#BSUB -q pdebug
+#BSUB -W 60
+
+#Loading cuda will cause linking error
+#module load cuda/11.4.1
+
+source anaconda/bin/activate
+conda activate opence-1.8.0
+
+python main_AE_GFINNs_simul.py
