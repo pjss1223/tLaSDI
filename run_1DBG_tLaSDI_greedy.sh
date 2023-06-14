e2df4d955e4520e452c7d91ca0d141c9ef82d7bd
Jun Sur Park <park47@lassen709.coral.llnl.gov>
2023-06-14 13:10:39 -0700

    change the run script

--- run_1DBG_tLaSDI_greedy.sh
+++ run_1DBG_tLaSDI_greedy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #BSUB -nnodes 1
-#BSUB -q pdebug
-#BSUB -W 60
+#BSUB -q pbatch
+#BSUB -W 120
 
 latent_dim="10"
 net="ESP3"  # 'ESP3' (GFINNs) or 'ESP3_soft' (SPNN)
-iterations="10"
+iterations="10000"
 # loss weights  (Integrator loss weight: 1)
 lambda_r_SAE="1e-1"  # reconstruction
 lambda_jac_SAE="1e-6"  # Jacobian
@@ -19,6 +19,6 @@ source anaconda/bin/activate
 conda activate opence-1.8.0
 
 TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S") 
-OUTPUT_PREFIX=${latent_dim}_${net}_${TIMESTAMP}
+OUTPUT_PREFIX=${latent_dim}_${net}_${lambda_r_SAE}_${lambda_jac_SAE}_${lambda_dx}_${lambda_dz}
 
 python main_1DBG_tLaSDI_greedy.py --latent_dim ${latent_dim} --net ${net} --iterations ${iterations} --lambda_r_SAE ${lambda_r_SAE} --lambda_jac_SAE ${lambda_jac_SAE} --lambda_dx ${lambda_dx} --lambda_dz ${lambda_dz} > ${OUTPUT_PREFIX}.log
