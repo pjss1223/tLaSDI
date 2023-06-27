b20afe88136cc6f8b198f084bea70e049dea55bf
Jun Sur Park <park47@lassen709.coral.llnl.gov>
2023-06-27 09:05:24 -0700

    averaged relative l2 error

--- run_VC_tLaSDI.sh
+++ run_VC_tLaSDI.sh
@@ -5,7 +5,7 @@
 
 problem="VC"
 latent_dim="6"
-net="ESP3"  # 'ESP3' (GFINNs) or 'ESP3_soft' (SPNN)
+net="ESP3_soft"  # 'ESP3' (GFINNs) or 'ESP3_soft' (SPNN)
 iterations="20000"
 # loss weights  (Integrator loss weight: 1)
 lambda_r_SAE="1e-1"  # reconstruction 1e-1
@@ -13,7 +13,7 @@ lambda_jac_SAE="1e-6"  # Jacobian 1e-6
 lambda_dx="1e-4" # Consistency 1e-4
 lambda_dz="1e-4" # Model approximation 1e-4
 
-load_model="True"
+load_model="False"
 load_iterations="20000" # total iterations before loaded
 
 
