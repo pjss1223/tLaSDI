fix normalizing for RT example

model.py
@@ -469,7 +469,13 @@ class StackedSparseAutoEncoder(nn.Module):
 
     # Normalization
     def normalize(self, z):
-        n_nodes = 4140
+        #print(z.shape[1])
+        if z.shape[1] == 12420:
+            n_nodes = 1035
+        elif z.shape[1] == 24840:
+            n_nodes = 2070
+        elif z.shape[1] == 49680:
+            n_nodes = 4140
         # Position
         q1_norm = z[:, n_nodes * 0:n_nodes * 1] / 1.5
         q2_norm = z[:, n_nodes * 1:n_nodes * 2] / 0.1