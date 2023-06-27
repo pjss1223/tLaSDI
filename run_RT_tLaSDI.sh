c26c14079dd7cc6482e52772f1574f53201020d4
Jun Sur Park <park47@lassen709.coral.llnl.gov>
2023-06-27 08:05:31 -0700

    modified dataset_sim.py

--- run_RT_tLaSDI.sh
+++ run_RT_tLaSDI.sh
@@ -4,17 +4,17 @@
 #BSUB -W 240
 
 problem="RT"
-latent_dim="2"
+latent_dim="10"
 net="ESP3"  # 'ESP3' (GFINNs) or 'ESP3_soft' (SPNN)
 iterations="30000"
 # loss weights  (Integrator loss weight: 1)
-lambda_r_SAE="1e-1"  # reconstruction 1e-1
+lambda_r_SAE="1e-3"  # reconstruction 1e-1
 lambda_jac_SAE="0"  # Jacobian 1e-6
-lambda_dx="1e-4" # Consistency 1e-4
-lambda_dz="1e-5" # Model approximation 1e-5
+lambda_dx="0" # Consistency 1e-4
+lambda_dz="0" # Model approximation 1e-5
 
 load_model="False"
-load_iterations="10" # total iterations before loaded
+load_iterations="30000" # total iterations before loaded
 
 #Loading cuda will cause linking error
 #module load cuda/11.4.1
